246401583446a439e0d1b6c8232c68e3ec8dc4c0
DavFount <47730893+DavFount@users.noreply.github.com>
2023-08-09 21:40:53 -0700

    Added optional groupId for prompts while targeting entity.

--- client/services/prompts.lua
+++ client/services/prompts.lua
@@ -1,18 +1,17 @@
 PromptsAPI = {}
-function PromptsAPI:SetupPromptGroup()
+function PromptsAPI:SetupPromptGroup(groupId)
     ----------------- Setup PromptGroup class and add attributes to prompt-----------------
     local GroupsClass = {}
-    GroupsClass.PromptGroup = GetRandomIntInRange(0, 0xffffff)
+    GroupsClass.PromptGroup = CheckVar(groupId, GetRandomIntInRange(0, 0xffffff))
     ----------------- ----------------- ----------------- ----------------- ----------
 
     ----------------- PromptGroup Specific APIs below -----------------
     function GroupsClass:ShowGroup(text)
-        PromptSetActiveGroupThisFrame(self.PromptGroup, CreateVarString(10, 'LITERAL_STRING', CheckVar(text, 'Prompt Info')))
+        PromptSetActiveGroupThisFrame(self.PromptGroup,
+            CreateVarString(10, 'LITERAL_STRING', CheckVar(text, 'Prompt Info')))
     end
 
-
     function GroupsClass:RegisterPrompt(title, button, enabled, visible, pulsing, mode, options)
-        
         ----------------- Setup Prompt class and add attributes to prompt-----------------
         local PromptClass = {}
         PromptClass.Prompt = PromptRegisterBegin()
@@ -34,7 +33,8 @@ function PromptsAPI:SetupPromptGroup()
 
         if mode == 'hold' then
             -- Posible hashes SHORT_TIMED_EVENT_MP, SHORT_TIMED_EVENT, MEDIUM_TIMED_EVENT, LONG_TIMED_EVENT, RUSTLING_CALM_TIMING, PLAYER_FOCUS_TIMING, PLAYER_REACTION_TIMING
-            Citizen.InvokeNative(0x74C7D7B72ED0D3CF, PromptClass.Prompt, CheckVar(options and options.timedeventhash, 'MEDIUM_TIMED_EVENT')) -- PromptSetStandardizedHoldMode
+            Citizen.InvokeNative(0x74C7D7B72ED0D3CF, PromptClass.Prompt,
+                CheckVar(options and options.timedeventhash, 'MEDIUM_TIMED_EVENT')) -- PromptSetStandardizedHoldMode
         end
 
         if mode == 'mash' then
@@ -43,15 +43,16 @@ function PromptsAPI:SetupPromptGroup()
 
         if mode == 'timed' then
             print('timed mode set!')
-            Citizen.InvokeNative(0x1473D3AF51D54276, PromptClass.Prompt, CheckVar(options and options.depletiontime, 10000))
+            Citizen.InvokeNative(0x1473D3AF51D54276, PromptClass.Prompt,
+                CheckVar(options and options.depletiontime, 10000))
         end
 
         PromptSetGroup(PromptClass.Prompt, self.PromptGroup)
         Citizen.InvokeNative(0xC5F428EE08FA7F2C, PromptClass.Prompt, CheckVar(pulsing, true))
         PromptRegisterEnd(PromptClass.Prompt)
-    
+
         ----------------- Prompt Specific APIs below -----------------
-         function PromptClass:TogglePrompt(toggle)
+        function PromptClass:TogglePrompt(toggle)
             PromptSetVisible(self.Prompt, toggle)
         end
 
@@ -67,26 +68,26 @@ function PromptsAPI:SetupPromptGroup()
             if self.Mode == 'click' then
                 return Citizen.InvokeNative(0xC92AC953F0A982AE, self.Prompt) --UiPromptHasStandardModeCompleted
             end
-    
+
             if self.Mode == 'hold' or self.Mode == 'customhold' then
                 local result = Citizen.InvokeNative(0xE0F65F0640EF0617, self.Prompt) --UiPromptHasHoldModeCompleted
 
-                if result then 
-                    Wait(500) --Prevents the spamming of the result (ensures it only gets triggered 1 time)
-                end
-    
-                return result
-            end
-    
-            if self.Mode == 'mash' then
-                local result = Citizen.InvokeNative(0x845CE958416DC473, self.Prompt) --UiPromptHasMashModeCompleted
-                if result then 
+                if result then
                     Wait(500) --Prevents the spamming of the result (ensures it only gets triggered 1 time)
                 end
 
                 return result
             end
-    
+
+            if self.Mode == 'mash' then
+                local result = Citizen.InvokeNative(0x845CE958416DC473, self.Prompt) --UiPromptHasMashModeCompleted
+                if result then
+                    Wait(500)                                                        --Prevents the spamming of the result (ensures it only gets triggered 1 time)
+                end
+
+                return result
+            end
+
             if self.Mode == 'timed' then
                 local result = Citizen.InvokeNative(0x3CE854D250A88DAF, self.Prompt) --UiPromptHasPressedTimedModeCompleted
 
@@ -103,17 +104,17 @@ function PromptsAPI:SetupPromptGroup()
             if self.Mode == 'click' or self.Mode == 'hold' or self.Mode == 'customhold' then
                 return false
             end
-    
+
             if self.Mode == 'mash' then
                 local result = Citizen.InvokeNative(0x25B18E530CF39D6F, self.Prompt) --UiPromptHasMashModeFailed
 
                 if result then
                     self:TogglePrompt(false)
                 end
-    
+
                 return result
             end
-    
+
             if self.Mode == 'timed' then
                 local result = Citizen.InvokeNative(0x1A17B9ECFF617562, self.Prompt) --UiPromptHasPressedTimedModeFailed
 
