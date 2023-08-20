c1c9262d9223dcd1d8d17d37f8cecac31bc108d5
AndrewR3K <10902965+AndrewR3K@users.noreply.github.com>
2023-08-19 22:23:57 -0700

    split radius into its own blip api route, move fixed, map addition

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -4,7 +4,6 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your RedM NUI ready Vue.js App!"/>
   </div>
 </template>
