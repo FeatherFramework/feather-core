a3f41bd0a2e1d220d8eae8cab96b21612fe83682
Jake2k4 <119366047+jakeyboi1@users.noreply.github.com>
2023-10-02 19:13:33 -0500

    Update main.lua
    did not know this was a thing

--- client/main.lua
+++ client/main.lua
@@ -12,8 +12,7 @@ function RunCore()
 end
 
 if GetCurrentResourceName() ~= "feather-core" then
-    print("ERROR feather-core failed to load if you are a player report this to the server owner, if you are the server owner Feather Core resource must be named feather-core")
-    print("Feather Core faled to load.")
+    error("ERROR feather-core failed to load if you are a player report this to the server owner, if you are the server owner Feather Core resource must be named feather-core")
 else
     RunCore()
 end
