486a948750c79a4868340490d8b3451215321859
Elzetia <cy.buniet@gmail.com>
2023-11-02 19:43:44 +0100

    Create fr_fr.lua (#24)
    add french trad

---
+++ locale/fr_fr.lua
@@ -0,0 +1,11 @@
+LocalesAPI.register("fr_fr", {
+    loadscreen_title = "Chargement du monde...",
+    loadscreen_subtitle = "Dans ces contrées arides du Far West, chaque instant de chargement s'entrelace aux légendes évoquées autour des braseros crépitants.",
+    loadscreen_signature = "-Quelqu'un probablement",
+    spawn_welcome = "Bienvenue à Feathercore!",
+    death_timer = "secondes restantes",
+    death_text = "Réapparition immédiate",
+    death_prompt = "Maintenir",
+    ui_settings_title = "Paramètres",
+    ui_settings_locale_title = "Langue"
+})
