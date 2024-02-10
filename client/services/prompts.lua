606b51ffbea50368d93c7e038a6dc8f17b463e5a
Elzetia <cy.buniet@gmail.com>
2024-02-10 07:01:50 +0100

    Update prompts : TabIndex and Double on the prompt

--- client/services/prompts.lua
+++ client/services/prompts.lua
@@ -1,76 +1,98 @@
 PromptsAPI = {}
+
 function PromptsAPI:SetupPromptGroup(groupId)
+
     ----------------- Setup PromptGroup class and add attributes to prompt-----------------
     local GroupsClass = {}
-    GroupsClass.PromptGroup = CheckVar(groupId, GetRandomIntInRange(0, 0xffffff))
+    GroupsClass.PromptGroup = { id = CheckVar(groupId, GetRandomIntInRange(0, 0xffffff)) }
+    GroupsClass.tabAmount = 1 -- Initialization with a default value for tabAmount
     ----------------- ----------------- ----------------- ----------------- ----------
 
     ----------------- PromptGroup Specific APIs below -----------------
     function GroupsClass:ShowGroup(text)
-        PromptSetActiveGroupThisFrame(self.PromptGroup,
-            CreateVarString(10, 'LITERAL_STRING', CheckVar(text, 'Prompt Info')))
+        PromptSetActiveGroupThisFrame(
+            self.PromptGroup.id,
+            CreateVarString(10, 'LITERAL_STRING', CheckVar(text, 'Prompt Info')), GroupsClass.tabAmount, 0)
+    end
+
+    function GroupsClass:GetPromptPage() -- returns the tab currently displayed
+        if PromptSetActiveGroupThisFrame(self.PromptGroup.id) < 0 then
+            return 0
+        end
+        return PromptSetActiveGroupThisFrame(self.PromptGroup.id)
+    end
+
+    function GroupsClass:SetTabAmount(number) -- updates the group tabAmount
+        if GroupsClass.tabAmount < number + 1 then
+            GroupsClass.tabAmount = number + 1
+        end
     end
 
     -- TODO: Make a distance check and only show the closest registered prompt (Ideally this should DRASTICALLY help reduce resource consumption)
-    function GroupsClass:RegisterPrompt(title, button, enabled, visible, pulsing, mode, options)
+    function GroupsClass:RegisterPrompt(title, button, enabled, visible, pulsing, mode, options, tabIndex)
         ----------------- Setup Prompt class and add attributes to prompt-----------------
         local PromptClass = {}
+        PromptClass.Prompt = {}
+        PromptClass.tabIndex = tabIndex or 0
         PromptClass.Prompt = PromptRegisterBegin()
         PromptClass.Mode = mode
         ----------------- ----------------- ----------------- ----------------- ----------
 
-        PromptSetControlAction(PromptClass.Prompt, CheckVar(button, 0x4CC0E2FE))
+        if type(button) == "table" then -- Manages multi-hash or hash alone
+            for _, btn in pairs(button) do
+                PromptSetControlAction(PromptClass.Prompt, CheckVar(btn, 0x4CC0E2FE))
+            end
+        else
+            PromptSetControlAction(PromptClass.Prompt, CheckVar(button, 0x4CC0E2FE))
+        end
+
         PromptSetText(PromptClass.Prompt, CreateVarString(10, 'LITERAL_STRING', CheckVar(title, 'Title')))
-        PromptSetEnabled(PromptClass.Prompt, CheckVar(enabled, 1))
-        PromptSetVisible(PromptClass.Prompt, CheckVar(visible, 1))
+        PromptSetEnabled(PromptClass.Prompt, CheckVar(enabled, true))
+        PromptSetVisible(PromptClass.Prompt, CheckVar(visible, true))
+        PromptSetGroup(PromptClass.Prompt, self.PromptGroup.id, PromptClass.tabIndex)
 
+        -- Mode-specific treatment
         if mode == 'click' then
-            PromptSetStandardMode(PromptClass.Prompt, 1)
-        end
-
-        if mode == 'customhold' then
-            Citizen.InvokeNative(0x94073D5CA3F16B7B, PromptClass.Prompt, CheckVar(options and options.holdtime, 3000)) -- UiPromptSetHoldMode
-        end
-
-        if mode == 'hold' then
-            -- Posible hashes SHORT_TIMED_EVENT_MP, SHORT_TIMED_EVENT, MEDIUM_TIMED_EVENT, LONG_TIMED_EVENT, RUSTLING_CALM_TIMING, PLAYER_FOCUS_TIMING, PLAYER_REACTION_TIMING
+            PromptSetStandardMode(PromptClass.Prompt, true)
+        elseif mode == 'customhold' then
+            Citizen.InvokeNative(0x94073D5CA3F16B7B, PromptClass.Prompt, CheckVar(options and options.holdtime, 3000))
+        elseif mode == 'hold' then
+             -- Possible hashes SHORT_TIMED_EVENT_MP, SHORT_TIMED_EVENT, MEDIUM_TIMED_EVENT, LONG_TIMED_EVENT, RUSTLING_CALM_TIMING, PLAYER_FOCUS_TIMING, PLAYER_REACTION_TIMING
             Citizen.InvokeNative(0x74C7D7B72ED0D3CF, PromptClass.Prompt,
-                CheckVar(options and options.timedeventhash, 'MEDIUM_TIMED_EVENT')) -- PromptSetStandardizedHoldMode
-        end
-
-        if mode == 'mash' then
+                CheckVar(options and options.timedeventhash, 'MEDIUM_TIMED_EVENT'))
+        elseif mode == 'mash' then
             Citizen.InvokeNative(0xDF6423BF071C7F71, PromptClass.Prompt, CheckVar(options and options.mashamount, 20))
-        end
-
-        if mode == 'timed' then
+        elseif mode == 'timed' then
             Citizen.InvokeNative(0x1473D3AF51D54276, PromptClass.Prompt,
                 CheckVar(options and options.depletiontime, 10000))
         end
 
-        PromptSetGroup(PromptClass.Prompt, self.PromptGroup)
+
+        self:SetTabAmount(PromptClass.tabIndex) -- Update tabAmount if necessary
+
         Citizen.InvokeNative(0xC5F428EE08FA7F2C, PromptClass.Prompt, CheckVar(pulsing, true))
         PromptRegisterEnd(PromptClass.Prompt)
 
         ----------------- Prompt Specific APIs below -----------------
         function PromptClass:TogglePrompt(toggle)
-            Citizen.InvokeNative(0x71215ACCFDE075EE, self.Prompt, toggle)
+            Citizen.InvokeNative(0x71215ACCFDE075EE, self.Prompt[PromptClass.tabIndex], toggle)
         end
 
         function PromptClass:EnabledPrompt(toggle)
-            PromptSetEnabled(self.Prompt, toggle)
+            PromptSetEnabled(self.Prompt[PromptClass.tabIndex], toggle)
         end
 
         function PromptClass:DeletePrompt()
-            Citizen.InvokeNative(0x00EDE88D4D13CF59, self.Prompt) -- UiPromptDelete
+            Citizen.InvokeNative(0x00EDE88D4D13CF59, self.Prompt[PromptClass.tabIndex]) -- UiPromptDelete
         end
 
         function PromptClass:HasCompleted(hideoncomplete)
             if self.Mode == 'click' then
-                return Citizen.InvokeNative(0xC92AC953F0A982AE, self.Prompt) --UiPromptHasStandardModeCompleted
+                return Citizen.InvokeNative(0xC92AC953F0A982AE, self.Prompt[PromptClass.tabIndex]) --UiPromptHasStandardModeCompleted
             end
 
             if self.Mode == 'hold' or self.Mode == 'customhold' then
-                local result = Citizen.InvokeNative(0xE0F65F0640EF0617, self.Prompt) --UiPromptHasHoldModeCompleted
+                local result = Citizen.InvokeNative(0xE0F65F0640EF0617, self.Prompt[PromptClass.tabIndex]) --UiPromptHasHoldModeCompleted
 
                 if result then
                     Wait(500) --Prevents the spamming of the result (ensures it only gets triggered 1 time)
@@ -80,16 +102,16 @@ function PromptsAPI:SetupPromptGroup(groupId)
             end
 
             if self.Mode == 'mash' then
-                local result = Citizen.InvokeNative(0x845CE958416DC473, self.Prompt) --UiPromptHasMashModeCompleted
+                local result = Citizen.InvokeNative(0x845CE958416DC473, self.Prompt[PromptClass.tabIndex]) --UiPromptHasMashModeCompleted
                 if result then
-                    Wait(500)                                                        --Prevents the spamming of the result (ensures it only gets triggered 1 time)
+                    Wait(500)                                                                              --Prevents the spamming of the result (ensures it only gets triggered 1 time)
                 end
 
                 return result
             end
 
             if self.Mode == 'timed' then
-                local result = Citizen.InvokeNative(0x3CE854D250A88DAF, self.Prompt) --UiPromptHasPressedTimedModeCompleted
+                local result = Citizen.InvokeNative(0x3CE854D250A88DAF, self.Prompt[PromptClass.tabIndex]) --UiPromptHasPressedTimedModeCompleted
 
                 if result and CheckVar(hideoncomplete, true) then
                     self:TogglePrompt(false)
@@ -106,7 +128,7 @@ function PromptsAPI:SetupPromptGroup(groupId)
             end
 
             if self.Mode == 'mash' then
-                local result = Citizen.InvokeNative(0x25B18E530CF39D6F, self.Prompt) --UiPromptHasMashModeFailed
+                local result = Citizen.InvokeNative(0x25B18E530CF39D6F, self.Prompt[PromptClass.tabIndex]) --UiPromptHasMashModeFailed
 
                 if result then
                     self:TogglePrompt(false)
@@ -116,7 +138,7 @@ function PromptsAPI:SetupPromptGroup(groupId)
             end
 
             if self.Mode == 'timed' then
-                local result = Citizen.InvokeNative(0x1A17B9ECFF617562, self.Prompt) --UiPromptHasPressedTimedModeFailed
+                local result = Citizen.InvokeNative(0x1A17B9ECFF617562, self.Prompt[PromptClass.tabIndex]) --UiPromptHasPressedTimedModeFailed
 
                 if result and CheckVar(hideoncomplete, true) then
                     self:TogglePrompt(false)
