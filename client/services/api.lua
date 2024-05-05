3ccf23abc1df8f173a301f0d42ddd3b684621836
AndrewR3K <10902965+AndrewR3K@users.noreply.github.com>
2024-05-04 17:34:04 -0700

    More minor bugfixes

--- client/services/api.lua
+++ client/services/api.lua
@@ -10,7 +10,7 @@ function StartAPI()
     CoreClientApi.Prompt = PromptsAPI
     CoreClientApi.Render = RenderAPI
     CoreClientApi.Wagon = WagonsAPI
-    CoreClientApi.KeysCodes = KeyPressAPI
+    CoreClientApi.KeyCodes = KeyPressAPI
     CoreClientApi.Clip = ClipAPI
 
     CoreClientApi = SetupSharedAPI(CoreClientApi)
