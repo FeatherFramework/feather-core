e966d63040bd0e72ef74c4e0bd3c696b1a216c7e
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-21 14:19:10 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ lua54 "yes"
 description 'The Core service for the Feather Framework'
 author 'Feather @Bytesizd'
 name 'feather-core'
-version '0.1.5'
+version '0.1.6'
 
 github_version_check 'true'
 github_version_type 'release'
